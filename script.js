91f19ded50245cd5b7f078962a92c5f92af6a5ad
NAGATOPain <nguyenluu2101@gmail.com>
2018-06-04 20:55:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -193,11 +193,12 @@ function timer() {
         var distance = countDownDate - now;
 
         // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Output the result in an element with id="demo"
-        document.getElementById("time").innerHTML = "Còn lại: " + minutes + " phút," + seconds + " giây.";
+        document.getElementById("time").innerHTML = "Còn lại: " +hour+" giờ, +"+ minutes + " phút," + seconds + " giây.";
 
         // If the count down is over, write some text 
         if (distance < 0) {
