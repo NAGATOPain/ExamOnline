a925e84868f714d18c9405249a6b92c36a87903d
NAGATOPain <nguyenluu2101@gmail.com>
2018-06-04 20:34:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var Name = "", Subject = "", Num = 0, Answer = "", Nameoftest = "", Time = 0;
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
         Subject = myObj.subject.toUpperCase();
@@ -16,27 +16,31 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "data.txt", true);
 xmlhttp.send();
 
-function indexButton(){
+function indexButton() {
     var name = document.getElementById("input-name").value.trim();
     Name = name;
     document.title = "Phiếu đáp án";
     document.getElementById("title").innerHTML = "<h1><b>PHIẾU ĐÁP ÁN</b></h1>";
     document.getElementById("panel").innerHTML = '';
-    window.open("https://github.com/NAGATOPain/NAGATOPain.github.io/raw/master/test.pdf", '_blank');
+    //window.open("https://github.com/NAGATOPain/NAGATOPain.github.io/raw/master/test.pdf", '_blank');
     generateQuiz();
 }
 
-function generateQuiz(){
+function generateQuiz() {
     var panel = document.getElementById("panel");
-    var i;
-    for (i = 0; i < Num; i++){
+    var time = document.createElement("div");
+    time.name = "time";
+    time.id = "label";
+    panel.appendChild(time);
+    timer();
+    for (var i = 0; i < Num; i++) {
         var quizAtI = document.createElement("div");
-        quizAtI.name = "as" + (i+1).toString();
+        quizAtI.name = "as" + (i + 1).toString();
         var lblNum = document.createElement("b");
         lblNum.id = "label";
         var str;
-        if (i < 9) str = "Câu 0"+(i+1).toString()+": ";
-        else str = "Câu "+(i+1).toString()+": ";
+        if (i < 9) str = "Câu 0" + (i + 1).toString() + ": ";
+        else str = "Câu " + (i + 1).toString() + ": ";
 
         var s = document.createTextNode(str);
         lblNum.appendChild(s);
@@ -45,12 +49,11 @@ function generateQuiz(){
         var quiz = document.createElement("span");
         quiz.id = "quiz";
         //Quiz setup:
-        var j;
         var buffer = "ABCD";
-        for (j = 0; j < 4; j++){
-            quiz.appendChild(document.createTextNode(buffer.charAt(j)+"."));
+        for (var j = 0; j < 4; j++) {
+            quiz.appendChild(document.createTextNode(buffer.charAt(j) + "."));
             var radio = document.createElement("input");
-            radio.type = "radio"; radio.name = "as" + (i+1).toString();
+            radio.type = "radio"; radio.name = "as" + (i + 1).toString();
             radio.id = "radio";
             quiz.appendChild(radio);
         }
@@ -59,34 +62,33 @@ function generateQuiz(){
     }
     //Add button
     var button = document.createElement("button");
-    button.id = "button"; 
-    button.type = "button"; 
+    button.id = "button";
+    button.type = "button";
     button.appendChild(document.createTextNode("Chấm điểm >>"));
     button.onclick = scoring;
     panel.appendChild(button);
 }
 
-function scoring(){
+function scoring() {
     var result = [];
     var answer = [];
-    var i;
-    for (i = 0; i < Num; i++){
+    for (var i = 0; i < Num; i++) {
         result.push(Answer.charAt(i));
         result[i] += "";
         result[i] = result[i].toLowerCase();
         //result[i] += "";
-        var checkbox = document.getElementsByName("as"+(i+1).toString());
-        var checkone,j;
-        for (j = 0; j < checkbox.length; j++){
-            if (checkbox[j].checked){
-                checkone = j+1; break;
+        var checkbox = document.getElementsByName("as" + (i + 1).toString());
+        var checkone;
+        for (var j = 0; j < checkbox.length; j++) {
+            if (checkbox[j].checked) {
+                checkone = j + 1; break;
             }
-            if (j == checkbox.length - 1 && !checkbox[checkbox.length - 1].checked){
+            if (j == checkbox.length - 1 && !checkbox[checkbox.length - 1].checked) {
                 alert("Bạn bỏ sót đáp án. Hãy chọn cho đủ !");
                 return;
             }
         }
-        switch (checkone){
+        switch (checkone) {
             case 1:
                 answer.push("a");
                 break;
@@ -102,28 +104,28 @@ function scoring(){
             default:
                 break;
         }
-    } 
+    }
     var score = 0.0;
     var wrong_ans = [];
 
-    for (i = 0; i < Num; i++){
-        if (answer[i] == result[i]) score += (10.0/Num);
-        else wrong_ans.push(i+1);
+    for (var i = 0; i < Num; i++) {
+        if (answer[i] == result[i]) score += (10.0 / Num);
+        else wrong_ans.push(i + 1);
     }
     var score_str = score.toFixed(2).toString();
     str = "";
-    if (wrong_ans.length == 0){
+    if (wrong_ans.length == 0) {
         str = "Bạn không sai câu nào !"
     }
-    else{
-        for (i = 0; i < wrong_ans.length - 1; i++)
-        str += wrong_ans[i].toString() + " - "+result[wrong_ans[i]-1].toUpperCase()+", ";
-        str += wrong_ans[wrong_ans.length - 1].toString() + " - "+result[wrong_ans[wrong_ans.length - 1]-1].toUpperCase()+".";
+    else {
+        for (var i = 0; i < wrong_ans.length - 1; i++)
+            str += wrong_ans[i].toString() + " - " + result[wrong_ans[i] - 1].toUpperCase() + ", ";
+        str += wrong_ans[wrong_ans.length - 1].toString() + " - " + result[wrong_ans[wrong_ans.length - 1] - 1].toUpperCase() + ".";
     }
     switchToResultPage(score_str, str);
 }
 
-function switchToResultPage(score, str){
+function switchToResultPage(score, str) {
     document.title = "Kết quả";
     document.getElementById("title").innerHTML = "<h1><b>KẾT QUẢ</b></h1>";
     document.getElementById("panel").innerHTML = '';
@@ -169,12 +171,39 @@ function switchToResultPage(score, str){
     panel.appendChild(document.createElement("br"));
     panel.appendChild(wrong_panel);
     panel.appendChild(document.createElement("br"));
-    
+
     //Add button
     var button = document.createElement("button");
     button.id = "button";
-    button.type = "button"; 
+    button.type = "button";
     button.appendChild(document.createTextNode("Trở lại trang chủ >>"));
-    button.onclick = () => {location.reload();};
+    button.onclick = () => { location.reload(); };
     panel.appendChild(button);
+}
+
+function timer() {
+    var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementByName("time").innerHTML = "Còn lại: " + minutes + " phút," + seconds + " giây.";
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementByName("time").innerHTML = "HẾT GIỜ !";
+        }
+    }, 1000);
 }
